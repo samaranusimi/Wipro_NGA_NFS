809df9abbb033dbf760b4fbee4fc56ac82181b95
samaranusimi <103650565+samaranusimi@users.noreply.github.com>
2026-04-25 17:18:45 +0530

    update

---
+++ Day_8/Encryption/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Encryption
+{
+    internal class Program
+    {
+        
+    }
+}
+
+
+
+/*static void Main(string[] args)
+        {
+            string Hashvalue = Hashpassword("Samar@123");
+            Console.WriteLine(Hashvalue);
+        }
+        public static string Hashpassword(string password)
+        {
+            using (SHA256 shal = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(password);
+                var hash = shal.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }*/
