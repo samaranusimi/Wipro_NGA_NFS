63bfb277bb137d53d09d94c0d926e65a0af2c512
samaranusimi <103650565+samaranusimi@users.noreply.github.com>
2026-04-27 16:25:34 +0530

    update

---
+++ Testing_demo/DemoApp.Tests/Test1.cs
@@ -0,0 +1,16 @@
+﻿using testDemo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace DemoApp.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var Calc = new Calculator();
+        int result = Calc.Multiply(7, 2);
+        Assert.AreEqual(14, result);
+
+    }
+}
---
+++ Testing_demo/testDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testDemo
+{
+    public class Calculator
+    {
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
