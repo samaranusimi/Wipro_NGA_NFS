08dc10df9d2b0171f7a25ce25f19f09b0e64d4be
samaranusimi <103650565+samaranusimi@users.noreply.github.com>
2026-04-29 17:36:09 +0530

    update

--- Assignment_Day_4/Scenario_4_Music_Playlist_Manager/Program.cs
+++ Assignment_Day_4/Scenario_4_Music_Playlist_Manager/Program.cs
@@ -5,32 +5,27 @@ class Program
 {
     static void Main()
     {
-        // Playlist
         LinkedList<string> playlist = new LinkedList<string>();
         playlist.AddLast("Tum Hi Ho");
         playlist.AddLast("Jeene Laga Hoon");
         playlist.AddLast("Tera Ban Jaunga");
 
-        // Ratings
         SortedList<int, string> ratings = new SortedList<int, string>();
         ratings.Add(5, "Tum Hi Ho");
         ratings.Add(4, "Jeene Laga Hoon");
         ratings.Add(3, "Tera Ban Jaunga");
 
-        // Artist → Song
         SortedDictionary<string, string> artists = new SortedDictionary<string, string>();
         artists.Add("Arijit Singh", "Tum Hi Ho");
         artists.Add("Atif Aslam", "Jeene Laga Hoon");
         artists.Add("Akhil Sachdeva", "Tera Ban Jaunga");
 
-        // Print Playlist
         Console.WriteLine("Playlist:");
         foreach (string song in playlist)
         {
             Console.WriteLine(song);
         }
 
-        // Remove a song
         playlist.Remove("Jeene Laga Hoon");
 
         Console.WriteLine("\nAfter removing a song:");
@@ -39,14 +34,12 @@ class Program
             Console.WriteLine(song);
         }
 
-        // Sorted by rating
         Console.WriteLine("\nSorted by Rating:");
         foreach (var item in ratings)
         {
             Console.WriteLine(item.Key + " : " + item.Value);
         }
 
-        // Sorted by artist
         Console.WriteLine("\nSorted by Artist:");
         foreach (var item in artists)
         {
