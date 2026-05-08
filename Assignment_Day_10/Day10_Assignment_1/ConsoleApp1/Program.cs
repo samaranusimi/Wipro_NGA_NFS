22b8c5796341596713c71a7233b6fb13a7cc4998
samaranusimi <103650565+samaranusimi@users.noreply.github.com>
2026-05-08 14:43:16 +0530

    Day10_UnitTesting_Assignment

---
+++ Assignment_Day_10/Day10_Assignment_1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CalculatorLibary
+{
+    public class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public double Divide(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException("Value of y can't be 0");
+            }
+            else
+            {
+                return x / y;
+            }
+
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Calculator App");
+        }
+
+    }
+}
---
+++ Assignment_Day_10/Day10_Assignment_1/TestProject1/Test1.cs
@@ -0,0 +1,41 @@
+﻿using CalculatorLibary;
+namespace TestProject;
+
+[TestClass]
+public sealed class Test1
+{
+    private Calculator Calc;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        Calc = new Calculator();
+    }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+        double result = Calc.Multiply(7, 2);
+        Assert.AreEqual(14, result);
+    }
+    [TestMethod]
+    public void TestMethod2()
+    {
+        double result = Calc.Add(7, 2);
+        Assert.AreEqual(9, result);
+    }
+
+    [TestMethod]
+    public void TestMethod3()
+    {
+        double result = Calc.Add(9, 2);
+        Assert.AreEqual(11, result);
+    }
+
+    [TestMethod]
+    public void TestMethod4()
+    {
+        double result = Calc.Divide(30, 2);
+        Assert.AreEqual(15, result);
+    }
+}
